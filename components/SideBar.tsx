70a17c3e10074c83af8e3a50c7e96f3d14d27ea5
jacob143590 <jacob143590@gmail.com>
2023-12-04 14:54:20 +0100

    init

---
+++ components/SideBar.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { collection, orderBy, query } from "firebase/firestore";
+import { signOut, useSession } from "next-auth/react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../firebase/firebase";
+import ChatRow from "./ChatRow";
+import ModelSelecetion from "./ModelSelecetion";
+import NewChat from "./NewChat";
+
+const SideBar = () => {
+  const { data: session } = useSession();
+
+  const [chat, loading, error] = useCollection(
+    session &&
+      query(
+        collection(db, "users", session.user?.email!, "chats"),
+        orderBy("createdAt", "asc")
+      )
+  );
+
+  return (
+    <div className="p-2 flex flex-col h-screen">
+      <div className="flex-1">
+        <div>
+          <div className="hidden sm:inline">
+            <NewChat />
+            <ModelSelecetion />
+          </div>
+          <div className="flex flex-col space-y-2 my-2">
+            {loading && (
+              <div className="animate-pulse text-center text-white">
+                <p>Loading Chats...</p>
+              </div>
+            )}
+
+            {chat?.docs.map((chat) => (
+              <ChatRow key={chat.id} id={chat.id} />
+            ))}
+          </div>
+        </div>
+      </div>
+      {session && (
+        <img
+          onClick={() => signOut()}
+          src={session.user?.image!}
+          alt="profile-pic"
+          className="h-12 w-12 rounded-full mx-auto mb-2 hover:opacity-50 cursor-pointer"
+        />
+      )}
+    </div>
+  );
+};
+
+export default SideBar;
---
+++ pages/api/askQuestion.ts
@@ -0,0 +1,47 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import query from "../../lib/queryApi";
+import admin from "firebase-admin";
+import { adminDB } from "../../firebase/firebaseAdmin";
+
+type Data = {
+  answer: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { prompt, chatId, model, session } = req.body;
+
+  if (!prompt) {
+    res.status(400).json({ answer: "Please provide a prompt!" });
+  }
+
+  if (!chatId) {
+    res.status(400).json({ answer: "Please provide valid Chat ID!" });
+  }
+  //? ChatGPT Query
+  const response = await query(prompt, chatId, model);
+
+  const message: Message = {
+    text: response || "ChatGPT was unable to find an answer for that!",
+    createdAt: admin.firestore.Timestamp.now(),
+    user: {
+      _id: "ChatGPT",
+      name: "ChatGPT",
+      avatar:
+        "https://brandlogovector.com/wp-content/uploads/2023/01/ChatGPT-Icon-Logo-PNG.png",
+    },
+  };
+
+  await adminDB
+    .collection("users")
+    .doc(session?.user?.email)
+    .collection("chats")
+    .doc(chatId)
+    .collection("messages")
+    .add(message);
+
+  res.status(200).json({ answer: message.text });
+}
